e800757f0ba761dc86d6e5213b0bff95e343e019
Sebastien Deleuze <sdeleuze@pivotal.io>
2016-02-10 16:16:30 +0100

    Improve part 09 comments

--- src/test/java/io/pivotal/literx/Part09BlockingToReactive.java
+++ src/test/java/io/pivotal/literx/Part09BlockingToReactive.java
@@ -1,6 +1,7 @@
 package io.pivotal.literx;
 
 import java.util.Iterator;
+import java.util.concurrent.Callable;
 
 import io.pivotal.literx.domain.User;
 import io.pivotal.literx.repository.BlockingRepository;
@@ -12,12 +13,21 @@ import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.SchedulerGroup;
 import reactor.core.test.TestSubscriber;
 
 /**
- * Learn how to call blocking code for Reactive one.
+ * Learn how to call blocking code from Reactive one with adapted concurrency strategy for
+ * blocking code that produces data (publishOn) or receives data (dispatchOn).
+ *
+ * For those who already know RxJava:
+ *  - RxJava subscribeOn = Reactor publishOn
+ *  - RxJava observeOn = Reactor dispatchOn
  *
  * @author Sebastien Deleuze
+ * @see Flux#publishOn(Callable)
+ * @see Flux#dispatchOn(Callable)
+ * @see SchedulerGroup
  */
 public class Part09BlockingToReactive {
 
@@ -36,7 +46,7 @@ public class Part09BlockingToReactive {
 				.assertComplete();
 	}
 
-	// TODO Create a Flux for reading all users from the blocking repository, and run it with a scheduler factory suitable for slow blocking request methods
+	// TODO Create a Flux for reading all users from the blocking repository, and run it with a scheduler factory suitable for slow {@link Runnable} executions
 	Flux<User> blockingRepositoryToFlux(BlockingRepository<User> repository) {
 		return null;
 	}
@@ -62,7 +72,7 @@ public class Part09BlockingToReactive {
 		assertFalse(it.hasNext());
 	}
 
-	// TODO Insert values in the blocking repository using an scheduler factory suitable for blocking but fast onNext methods
+	// TODO Insert users contained in the Flux parameter in the blocking repository using an scheduler factory suitable for fast {@link Runnable} executions
 	Mono<Void> fluxToBlockingRepository(Flux<User> flux, BlockingRepository<User> repository) {
 		return null;
 	}
