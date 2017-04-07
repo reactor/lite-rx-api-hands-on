4c688ab378a07adafd68bd40494a28ab4fc2e843
Stephane Maldini <smaldini@pivotal.io>
2017-04-07 15:14:02 +0100

    Update Part11 t use appropriate Scheduler

--- src/test/java/io/pivotal/literx/Part11BlockingToReactive.java
+++ src/test/java/io/pivotal/literx/Part11BlockingToReactive.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2011-2017 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.pivotal.literx;
 
 import java.util.Iterator;
@@ -23,6 +38,7 @@ import reactor.test.StepVerifier;
  * For those who know RxJava:
  *  - RxJava subscribeOn = Reactor subscribeOn
  *  - RxJava observeOn = Reactor publishOn
+ *  - RxJava Schedulers.io <==> Reactor Schedulers.elastic
  *
  * @author Sebastien Deleuze
  * @see Flux#subscribeOn(Scheduler)
@@ -70,8 +86,8 @@ public class Part11BlockingToReactive {
 	// TODO Insert users contained in the Flux parameter in the blocking repository using an parallel scheduler and return a Mono<Void> that signal the end of the operation
 	Mono<Void> fluxToBlockingRepository(Flux<User> flux, BlockingRepository<User> repository) {
 		return flux
-				.publishOn(Schedulers.parallel())
-				.doOnNext(user -> repository.save(user))
+				.publishOn(Schedulers.elastic())
+				.doOnNext(repository::save)
 				.then(); // TO BE REMOVED
 	}
 
