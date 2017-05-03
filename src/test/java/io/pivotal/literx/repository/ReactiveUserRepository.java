41f47aacb85d05a4b2387082726c1b5d5013f84e
Simon Baslé <sbasle@pivotal.io>
2017-05-03 17:59:56 +0200

    Bump the "master" branch to reactor 3.1.0.M1, use BOM
     - `Mono.then(Function)` has been renamed to `flatMap`
     - note the old `Mono.flatMap` which returns a `Flux` is now named
      `flatMapMany`
    
    Now uses the Bismuth-M1 BOM.
    
    use the newly released BOM for 3.1.0.M1

--- src/test/java/io/pivotal/literx/repository/ReactiveUserRepository.java
+++ src/test/java/io/pivotal/literx/repository/ReactiveUserRepository.java
@@ -65,7 +65,7 @@ public class ReactiveUserRepository implements ReactiveRepository<User> {
 	private Mono<User> withDelay(Mono<User> userMono) {
 		return Mono
 				.delay(Duration.ofMillis(delayInMs))
-				.then(c -> userMono);
+				.flatMap(c -> userMono);
 	}
 
 	private Flux<User> withDelay(Flux<User> userFlux) {
