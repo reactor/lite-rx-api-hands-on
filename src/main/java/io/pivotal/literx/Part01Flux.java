ca1966782308d3a2b692ff6ecae97ed170469c4f
Simon Baslé <sbasle@pivotal.io>
2017-07-12 17:09:06 +0200

    Pre-import some packages to improve experience on simple editor

--- src/main/java/io/pivotal/literx/Part01Flux.java
+++ src/main/java/io/pivotal/literx/Part01Flux.java
@@ -1,5 +1,10 @@
 package io.pivotal.literx;
 
+//generic imports to help with simpler IDEs (ie tech.io)
+import java.util.*;
+import java.util.function.*;
+import java.time.*;
+
 import reactor.core.publisher.Flux;
 
 /**
