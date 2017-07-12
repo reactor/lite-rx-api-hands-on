ba4283505c6ad00e390e862f91bee27656d1e92f
Simon Baslé <sbasle@pivotal.io>
2017-07-12 17:15:02 +0200

    Clarify that StepVerifier 3600s test needs time manipulation

--- src/main/java/io/pivotal/literx/Part03StepVerifier.java
+++ src/main/java/io/pivotal/literx/Part03StepVerifier.java
@@ -60,8 +60,8 @@ public class Part03StepVerifier {
 
 //========================================================================================
 
-	// TODO Expect 3600 elements then complete using the virtual time capabilities provided
-	// via StepVerifier#withVirtualTime and notice how long the test takes
+	// TODO Expect 3600 elements at intervals of 1 second, and verify quicker than 3600s
+	// by manipulating virtual time thanks to StepVerifier#withVirtualTime, notice how long the test takes
 	void expect3600Elements(Supplier<Flux<Long>> supplier) {
 		fail();
 	}
