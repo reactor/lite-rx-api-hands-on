dd565d7b12e3f998d5ef091921edef01149a5fe9
Stephane Maldini <smaldini@pivotal.io>
2017-08-02 11:57:49 -0700

    Remove references to Hooks.onOperator

--- src/test/java/io/pivotal/literx/Part07ErrorsTest.java
+++ src/test/java/io/pivotal/literx/Part07ErrorsTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright (c) 2011-2017 Pivotal Software Inc, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,13 +16,10 @@
 
 package io.pivotal.literx;
 
-import java.util.function.Function;
-
 import io.pivotal.literx.domain.User;
 import org.junit.Test;
 import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Hooks;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -31,7 +28,6 @@ import reactor.test.StepVerifier;
  *
  * @author Sebastien Deleuze
  * @see Exceptions#propagate(Throwable)
- * @see Hooks#onOperator(Function)
  */
 public class Part07ErrorsTest {
 
