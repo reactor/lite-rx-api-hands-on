eb79220bdf14e54581a69930525188db3c8940f2
Eric Rong <erong@vmware.com>
2017-12-13 23:24:04 -0800

    Part 02 - Mono

--- src/main/java/io/pivotal/literx/Part02Mono.java
+++ src/main/java/io/pivotal/literx/Part02Mono.java
@@ -12,30 +12,30 @@ public class Part02Mono {
 
 //========================================================================================
 
-	// TODO Return an empty Mono
-	Mono<String> emptyMono() {
-		return null;
-	}
+    // TODO Return an empty Mono
+    Mono<String> emptyMono() {
+        return Mono.empty();
+    }
 
 //========================================================================================
 
-	// TODO Return a Mono that never emits any signal
-	Mono<String> monoWithNoSignal() {
-		return null;
-	}
+    // TODO Return a Mono that never emits any signal
+    Mono<String> monoWithNoSignal() {
+        return Mono.never();
+    }
 
 //========================================================================================
 
-	// TODO Return a Mono that contains a "foo" value
-	Mono<String> fooMono() {
-		return null;
-	}
+    // TODO Return a Mono that contains a "foo" value
+    Mono<String> fooMono() {
+        return Mono.just("foo");
+    }
 
 //========================================================================================
 
-	// TODO Create a Mono that emits an IllegalStateException
-	Mono<String> errorMono() {
-		return null;
-	}
+    // TODO Create a Mono that emits an IllegalStateException
+    Mono<String> errorMono() {
+        return Mono.error(new IllegalStateException());
+    }
 
 }
