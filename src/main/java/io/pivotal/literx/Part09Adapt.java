2a06a69d629e2a3311fa502fcb5b7bcaae8ad3ed
Simon Baslé <sbasle@pivotal.io>
2019-03-12 15:47:02 +0100

    polish a few imports and method reference

--- src/main/java/io/pivotal/literx/Part09Adapt.java
+++ src/main/java/io/pivotal/literx/Part09Adapt.java
@@ -19,8 +19,6 @@ package io.pivotal.literx;
 import java.util.concurrent.CompletableFuture;
 
 import io.pivotal.literx.domain.User;
-import io.pivotal.literx.repository.ReactiveRepository;
-import io.pivotal.literx.repository.ReactiveUserRepository;
 import io.reactivex.Flowable;
 import io.reactivex.Observable;
 import io.reactivex.Single;
@@ -40,8 +38,6 @@ import reactor.core.publisher.Mono;
  * @author Sebastien Deleuze
  */
 public class Part09Adapt {
-
-	ReactiveRepository<User> repository = new ReactiveUserRepository();
 
 //========================================================================================
 
