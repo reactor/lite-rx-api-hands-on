f2a52500137b05decc4705093293cc00aa34b8f5
Simon Baslé <sbasle@pivotal.io>
2019-03-12 16:01:12 +0100

    Redo the solution branch as `solution`, branched off master

--- src/main/java/io/pivotal/literx/Part08OtherOperations.java
+++ src/main/java/io/pivotal/literx/Part08OtherOperations.java
@@ -17,42 +17,44 @@ public class Part08OtherOperations {
 
 	// TODO Create a Flux of user from Flux of username, firstname and lastname.
 	Flux<User> userFluxFromStringFlux(Flux<String> usernameFlux, Flux<String> firstnameFlux, Flux<String> lastnameFlux) {
-		return null;
+		return Flux
+				.zip(usernameFlux, firstnameFlux, lastnameFlux)
+				.map(tuple -> new User(tuple.getT1(), tuple.getT2(), tuple.getT3())); // TO BE REMOVED
 	}
 
 //========================================================================================
 
 	// TODO Return the mono which returns its value faster
 	Mono<User> useFastestMono(Mono<User> mono1, Mono<User> mono2) {
-		return null;
+		return Mono.first(mono1, mono2); // TO BE REMOVED
 	}
 
 //========================================================================================
 
 	// TODO Return the flux which returns the first value faster
 	Flux<User> useFastestFlux(Flux<User> flux1, Flux<User> flux2) {
-		return null;
+		return Flux.first(flux1, flux2); // TO BE REMOVED
 	}
 
 //========================================================================================
 
 	// TODO Convert the input Flux<User> to a Mono<Void> that represents the complete signal of the flux
 	Mono<Void> fluxCompletion(Flux<User> flux) {
-		return null;
+		return flux.then(); // TO BE REMOVED
 	}
 
 //========================================================================================
 
 	// TODO Return a valid Mono of user for null input and non null input user (hint: Reactive Streams do not accept null values)
 	Mono<User> nullAwareUserToMono(User user) {
-		return null;
+		return Mono.justOrEmpty(user); // TO BE REMOVED
 	}
 
 //========================================================================================
 
 	// TODO Return the same mono passed as input parameter, expect that it will emit User.SKYLER when empty
 	Mono<User> emptyToSkyler(Mono<User> mono) {
-		return null;
+		return mono.defaultIfEmpty(User.SKYLER); // TO BE REMOVED
 	}
 
 //========================================================================================
