929da7da5606c6265a9c860a64e446e877cbb65b
Simon Baslé <sbasle@vmware.com>
2020-09-21 15:39:34 +0200

    fix #68 Make use of onErrorReturn in part 7 solution

--- src/main/java/io/pivotal/literx/Part07Errors.java
+++ src/main/java/io/pivotal/literx/Part07Errors.java
@@ -33,7 +33,7 @@ public class Part07Errors {
 
 	// TODO Return a Mono<User> containing User.SAUL when an error occurs in the input Mono, else do not change the input Mono.
 	Mono<User> betterCallSaulForBogusMono(Mono<User> mono) {
-		return mono.onErrorResume(e -> Mono.just(User.SAUL)); // TO BE REMOVED
+		return mono.onErrorReturn(User.SAUL); // TO BE REMOVED
 	}
 
 //========================================================================================
