7525e5a209ce1037f09d6fe17dd61391892c32fb
Paweł Pelczar <paweusz@gmail.com>
2020-09-21 09:48:25 +0200

    fix #75 Add Flux.collect exercise (#77)

--- src/main/java/io/pivotal/literx/Part08OtherOperations.java
+++ src/main/java/io/pivotal/literx/Part08OtherOperations.java
@@ -4,6 +4,8 @@ import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 /**
  * Learn how to use various other operators.
  *
@@ -50,6 +52,13 @@ public class Part08OtherOperations {
 
 	// TODO Return the same mono passed as input parameter, expect that it will emit User.SKYLER when empty
 	Mono<User> emptyToSkyler(Mono<User> mono) {
+		return null;
+	}
+
+//========================================================================================
+
+	// TODO Convert the input Flux<User> to a Mono<List<User>> containing list of collected flux values
+	Mono<List<User>> fluxCollection(Flux<User> flux) {
 		return null;
 	}
 
