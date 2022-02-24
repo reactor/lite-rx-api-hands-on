496750b8750e0c25b619a12bea7ee8e2f3c2d472
Simon Baslé <sbasle@vmware.com>
2022-02-24 16:47:17 +0100

    Part 06 should avoid doOnSubscribe, prefer doFirst (#90)

--- src/main/java/io/pivotal/literx/Part06Request.java
+++ src/main/java/io/pivotal/literx/Part06Request.java
@@ -38,7 +38,7 @@ public class Part06Request {
 
 //========================================================================================
 
-	// TODO Return a Flux with all users stored in the repository that prints "Starring:" on subscribe, "firstname lastname" for all values and "The end!" on complete
+	// TODO Return a Flux with all users stored in the repository that prints "Starring:" at first, "firstname lastname" for all values and "The end!" on complete
 	Flux<User> fluxWithDoOnPrintln() {
 		return null;
 	}
